978f838843161646744173030347bb3009cab012
Ashish <1ashishthakur2002@gmail.com>
2025-06-23 00:32:17 +0530

    changes

---
+++ index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+
+const userData = [
+    {
+        name: "ashish",
+        age: 23,
+        emaai: "ashish@patel.com",
+        Role: "SDE",
+    },
+    {
+        name: "shivam",
+        age: 23,
+        emaai: "shivasing@gmail.com",
+        Role: "web D",
+    },
+    {
+        name: "vish",
+        age: 23,
+        emaai: "vish@gmail.com",
+        Role: "Backend",
+    },
+]
+http.createServer((req, res) => {
+
+    res.write(JSON.stringify(userData));
+    res.end();
+}).listen(8000);
