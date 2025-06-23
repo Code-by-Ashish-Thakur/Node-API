045843767f693c65ade4b0686420fcdb67d888fa
Ashish <1ashishthakur2002@gmail.com>
2025-06-23 10:58:20 +0530

    change

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
+const express = require('express');
 require('dotenv').config();
+const bodyParser = require('body-parser');
 const mysql = require('mysql2');
+const bcrypt = require("bcrypt");
+const cors = require('cors');
+
+const PORT = 8001;
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
 
 
 const connection = mysql.createConnection({
@@ -9,10 +19,35 @@ const connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
+
+// now time to write the API 
+
+// -> This api to send the data in mysql 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 connection.connect((err) => {
     if (err) throw err;
     console.log("Database connected succesfully ");
-})
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+
 
 
 
