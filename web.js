34bceb2c1312a0cf9b79be929a47270d6bff26bd
Ashish <1ashishthakur2002@gmail.com>
2025-06-26 09:34:49 +0530

    file created page has been successfully

--- web.js
+++ web.js
@@ -1,17 +1,41 @@
 const http = require('http');
 const fs = require('fs');
-
+const queryString = require('querystring');
+const { log } = require('console');
 http.createServer((req, res) => {
 
     fs.readFile('html/web.html', 'utf-8', (err, data) => {   // utf-8 yaha par charcter ka type hai html ke andr
 
         if (err) {
-            res.writeHead(500, { "content-type": "text/html" })
+            res.writeHead(500, { "content-type": "text/plain" })
             res.write("Error a gaya hai yrr", err);
             res.end();
         }
-        res.writeHead(200, { "content-type": "text/html" })    // jb hm html ka content send krta hai tb header jaruri hota hai 
-        res.write(data);
+        res.writeHead(201, { "content-type": "text/html" })    // jb hm html ka content send krta hai tb header jaruri hota hai 
+
+        if (req.url == '/') {
+            res.write(data);
+        }
+        else if (req.url == '/submit') {
+            let dataBody = [];
+
+            req.on('data', (chunk) => {
+                dataBody.push(chunk);
+            });
+
+            req.on('end', () => {
+                let rawData = Buffer.concat(dataBody).toString();
+                let readData = queryString.parse(rawData);
+                console.log(readData);
+
+                let dataString = "My name is " + readData.name + " and my email is " + readData.email;
+
+                fs.writeFileSync("text/" + readData.name + ".txt", dataString);
+                console.log("file created successfully");
+            });
+            res.write("<h1>Data submit ho gaya hai </h1>")
+        }
+
         res.end();
     });
 
